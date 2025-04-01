d95acc5786f6ba7717e2f38f546cbad8e565943b
Попов Никита Михайлович <popovn@reshenie-soft.ru>
2025-04-01 18:18:14 +0300

    Добавил модули shopping-store, shopping-cart, warehouse

---
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApplication.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum;
+
+public class ShoppingCartApplication {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApplication.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum;
+
+public class ShoppingStoreApplication {
+    public static void main(String[] args) {
+
+    }
+}
